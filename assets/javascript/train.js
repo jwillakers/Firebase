341ad680a74c33f5968a66bbd16bbb4496d4d0be
Jennifer Williams <jwillakers@hotmail.com>
2018-01-21 18:49:27 -0600

    final

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -30,33 +30,120 @@
     // 16 % 7 = 2 (Modulus is the remainder)
     // 7 - 2 = 5 minutes away
     // 5 + 3:16 = 3:21
-
-    // Assumptions
+alert("hi")
+// Assumptions
     var tFrequency = 3;
 
-    // Time is 3:30 AM
+// Time is 3:30 AM
     var firstTime = "03:30";
 
-    // First Time (pushed back 1 year to make sure it comes before current time)
+// First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
     console.log(firstTimeConverted);
 
-    // Current Time
+// Current Time
     var currentTime = moment();
     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-    // Difference between the times
+// Difference between the times
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
     console.log("DIFFERENCE IN TIME: " + diffTime);
 
-    // Time apart (remainder)
+// Time apart (remainder)
     var tRemainder = diffTime % tFrequency;
     console.log(tRemainder);
 
-    // Minute Until Train
+// Minute Until Train
     var tMinutesTillTrain = tFrequency - tRemainder;
     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-    // Next Train
+
+// Next Train
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
     console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+//Variable to figure out the converted train time
+  // var trainTimeConverted = moment(trainTime, "HH:mm");
+
+// add Firebase...this code comes from the Firebase API I created here:Https://Console.Firebase.Google.Com/
+  
+  var config = {
+    apiKey: "AIzaSyABvE85oypqliNt5czdh6dA-U0Q0lmSpXY",
+    authDomain: "train-tracker-5e16c.firebaseapp.com",
+    databaseURL: "https://train-tracker-5e16c.firebaseio.com",
+    projectId: "train-tracker-5e16c",
+    storageBucket: "train-tracker-5e16c.appspot.com",
+    messagingSenderId: "404276825717"
+  };
+// Initialize Firebase
+  firebase.initializeApp(config);
+
+  var traindata = firebase.database();
+
+  // when submit button clicked
+  $("#click-button").on("click",function(){
+// prevent page from refreshing
+    event.preventDefault();
+
+// gets user input and trims off any extra spaces
+      var trainName = $("#train-name-input").val().trim();
+      var trainDestination = $("#train-destination-input").val().trim();
+      var trainTime = moment($("#train-start-input").val().trim(), "HH:mm").format("HH:mm");
+      var trainFrequency = $("#rate-input").val().trim();
+
+// Creates local "temporary" object for holding inputs
+      var newTrain = {
+        train: trainName,
+        destination: trainDestination,
+        first: trainTime,
+        frequency: trainFrequency
+    };
+//Setting the new values in the database
+    traindata.ref().push(newTrain);
+    
+// testing
+    console.log(newTrain.train);
+    console.log(newTrain.destination);
+    console.log(newTrain.first);
+    console.log(newTrain.frequency);
+    alert("train added")
+    
+    //Clearing the inputs
+     $("#train-name-input").val("");
+     $("#train-destination-input").val("");
+     $("#train-start-input").val("");
+     $("#rate-input").val("");
+});
+
+//Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+traindata.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  console.log(childSnapshot.val());
+    
+// Store everything into a variable.
+  var trainName = childSnapshot.val().train;
+  var trainDestination = childSnapshot.val().destination;
+  var firstTrainTime = childSnapshot.val().first;
+  var trainFrequency = childSnapshot.val().frequency;
+
+  var differenceTimes = moment().diff(firstTrainTime, "minutes");
+   //Variable to figure out the converted train time
+  var trainTimeConverted = moment(firstTrainTime, "HH:mm");
+    //Declaring a time difference variable
+  var timeDifference = moment().diff(moment(trainTimeConverted), "minutes");
+    console.log(timeDifference);
+    
+  var frequencyMinutes = childSnapshot.val().frequency;
+    console.log("Frequency Minutes: " + frequencyMinutes);
+  
+  var minutesAway = Math.abs(timeDifference % frequencyMinutes);
+    console.log("Minutes Away: " + minutesAway);
+  
+  var nextArrival = moment(currentTime).add(minutesAway, "minutes").format("hh:mm A");
+    console.log("Next Arrival: " + nextArrival);
+  
+    
+//Adding into the table
+$("#trainScheduleTable > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
+          trainFrequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+ });
